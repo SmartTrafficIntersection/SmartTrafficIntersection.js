a769a525fb859bba99a8a9efd23f8b20c89f4cd1
Ahmed R. Awadallah <aawadall@ualberta.ca>
2019-09-03 15:42:23 -0400

    define cell

---
+++ test/cell.spec.js
@@ -0,0 +1,5 @@
+/**
+* a cell will contain a road segment, a source, a sink, or a traffic controller 
+* a group of cells makes up a grid
+* cells should subscribe to events from adjacent cells 
+*/
